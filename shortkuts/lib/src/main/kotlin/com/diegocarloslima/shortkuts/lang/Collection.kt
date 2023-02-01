2f4892ce57f89e925afea23d2ac9ce422315fd94
Diego Lima <diegoclal@gmail.com>
2023-02-01 17:08:03 -0300

    Adding extension functions for CharSequence and Collection

---
+++ shortkuts/lib/src/main/kotlin/com/diegocarloslima/shortkuts/lang/Collection.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 Diego Carlos Lima <https://diegocarloslima.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.diegocarloslima.shortkuts.lang
+
+/**
+ * Convenience property for a nullable [Collection] object. It will return the size of the object or
+ * zero if the receiver is `null`.
+ *
+ * @see Collection.size
+ */
+inline val <T> Collection<T>?.size: Int get() = this?.size ?: 0
+
+/**
+ * Convenience property for a nullable [Collection] object. It will return `true` if the object is
+ * empty of if the receiver is `null`.
+ *
+ * @see Collection.isEmpty
+ */
+inline fun <T> Collection<T>?.isEmpty(): Boolean = this?.isEmpty() ?: true
+
+/**
+ * Convenience function for checking an element in a nullable [Collection] object.
+ *
+ * @param element [T] to be checked if its contained in the collection.
+ *
+ * @return `false` if the receiver is `null` of it the element is not contained in this collection.
+ * Returns `true` otherwise.
+ *
+ * @see Collection.contains
+ */
+inline operator fun <T> Collection<T>?.contains(element: @UnsafeVariance T): Boolean =
+    this?.contains(element) ?: false
+
+/**
+ * Convenience function for checking elements in a nullable [Collection] object.
+ *
+ * @param elements [T] to be checked if they are contained in the collection.
+ *
+ * @return `false` if the receiver is `null` or if the element is not contained in this collection.
+ * Returns `true` otherwise.
+ *
+ * @see Collection.containsAll
+ */
+inline fun <T> Collection<T>?.containsAll(elements: Collection<@UnsafeVariance T>): Boolean =
+    this?.containsAll(elements) ?: false
